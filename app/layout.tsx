9bed9bbefdf40e48362d626f64a1b32620e740d6
abhiisdev7 <madiabhilash1563@gmail.com>
2026-03-02 11:05:34 +0530

    fix: oxfmt for formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,26 @@
-import { ThemeProvider } from "@/components/context/theme-provider"
-import "@/css/globals.css"
-import React from "react"
+import React from 'react'
+
+import '@/css/globals.css'
+import { ThemeProvider } from '@/components/context/theme-provider'
 
 interface RootLayoutProps {
   children: React.ReactNode
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  return (<html lang="en" suppressHydrationWarning>
-    <head />
-    <body>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
-    </body>
-  </html>
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
   )
 }
